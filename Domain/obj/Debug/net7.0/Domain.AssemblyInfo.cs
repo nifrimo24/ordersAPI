3ac66dd02eda69cf1c52adb4eafe9448a54db568
Nicolás Morillo <nifrimo24@gmail.com>
2024-05-27 22:09:18 -0500

    add Unit Of Work interface

--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c950355c46321745678f0b043e8bf8c50cbc0726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ea440cee4e2a269af19f2e790a0dec623298d1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
